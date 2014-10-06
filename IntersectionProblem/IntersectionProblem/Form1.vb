dc3d8d5d928c48abe21d57f53550daf5f7e452fd
idsc3101 <idsc3101@gmail.com>
2014-10-06 15:25:40 -0500

    Complete

--- IntersectionProblem/IntersectionProblem/Form1.vb
+++ IntersectionProblem/IntersectionProblem/Form1.vb
@@ -25,9 +25,31 @@ Public Class Form1
         C = CDbl(Cst)
         D = CDbl(Dst)
 
-        ' Please write your code here...
+        If (B < A Or D < C) Then
+            MessageBox.Show("Please enter valid numbers", "Error!")
+            Return
+        End If
 
-        txtResult.Text = "Not implemented yet"
+        ' Please write your code here...
+        txtResult.Text = FindIntersection(A, B, C, D)
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Function FindIntersection(ByVal A As Double, B As Double, C As Double, D As Double) As String
+        Dim result As String
+        If (B < C) Or (D < A) Then
+            result = "AB does not intersect with CD"
+        Else
+            Dim begPoint As Double
+            begPoint = Math.Max(A, C)
+            Dim endPoint As Double
+            endPoint = Math.Min(B, D)
+            result = "The intersection is from " & begPoint & " to " & endPoint
+        End If
+        Return result
+    End Function
 
 End Class
